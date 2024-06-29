b80e586efa3390e5e8301cb558509302c31b9178
Nidhi Mishra <113460614+Nidhiiiim@users.noreply.github.com>
2024-06-29 17:52:28 -0400

    HashMap Implementation

---
+++ HashMaps.py
@@ -0,0 +1,34 @@
+# Initialising Dict or hashmap
+state_visited = {}
+
+states = ["New York", "New Jersey", "Kansas"]
+# First insert Key
+# Assign empty array everytime
+state_visited["America"] = []
+state_visited["America"].append(states)
+state_visited["America"].append("Omaha")
+print(state_visited)
+
+# Default dict initialized empty array for elements to be added.
+
+from collections import defaultdict
+# Assigned array automatically for all keys
+city_visited = defaultdict(list)
+
+NYcities = ["NYC", "Bronx"]
+NJcities = ["Princeton", "Jersey City"]
+MOCities = ["Kansas City"]
+city_visited["New York"] += NYcities
+city_visited["New Jersey"].append(NJcities)
+city_visited["Kansas"].append(MOCities)
+
+print(city_visited)
+
+# Extracting values
+
+print(city_visited.keys())
+print(city_visited.values())
+print(city_visited.items())
+
+
+
